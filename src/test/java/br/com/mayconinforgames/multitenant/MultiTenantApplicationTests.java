1fc9149c072b17621c75a11f08a398dd805b8167
maycon-santana <115080830+maycon-santana@users.noreply.github.com>
2024-05-11 17:34:02 -0300

    Criando projeto

---
+++ src/test/java/br/com/mayconinforgames/multitenant/MultiTenantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayconinforgames.multitenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
